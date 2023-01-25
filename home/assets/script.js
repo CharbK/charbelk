75212e2510999bc4ca8cd872fa9dc9143b314744
Charbel <charbel.kassab.08@gmail.com>
2023-01-25 17:35:22 -0500

    Animations

--- home/assets/script.js
+++ home/assets/script.js
@@ -1,1 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
 
+const hiddenElements1 = document.querySelectorAll(".hidden1");
+const hiddenElements2 = document.querySelectorAll(".hidden2");
+hiddenElements1.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer.observe(el));
